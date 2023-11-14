3ec38fee9c4ee71ad3492bec99c7f5288a7d06d2
Dane Jackson <danedjackson@yahoo.com>
2023-11-14 14:43:07 -0500

    Initial changes to convert console application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace trnservice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Text;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace trnservice
 {
@@ -10,37 +14,45 @@ namespace trnservice
     {
         static void Main(string[] args)
         {
-            String cTrn;
-            String[] ltrn;
-            Console.WriteLine("Hello World!");
-         
 
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        public static void Temp()
+        {
+            string[] ltrn;
+            //Console.WriteLine("Hello World!");
 
             ServiceReference1.MLSSServicesTRNClient obj = new ServiceReference1.MLSSServicesTRNClient();
-            //var objtrn = obj.GetIndividualTrnAsync(111734878).Result;
-            //Console.WriteLine(objtrn);
 
             StringBuilder sb = new StringBuilder();
 
-            foreach (String line in File.ReadLines(@"C:\Users\robinsonod\Downloads\Reports\WestmorelandWestern17-05-2023remainder.csv"))
+            foreach (string line in File.ReadLines(@"C:\Users\robinsonod\Downloads\Reports\WestmorelandWestern17-05-2023remainder.csv"))
             {
 
                 ltrn = line.Split(',');
                 string v = ltrn[1].ToString();
-                string test = v;
+                // Converted to char away to check if all characters are numeric
                 v.ToCharArray();
 
 
                 if (ltrn[1] == "" || (!v.All(char.IsDigit)))
-
                 {
+                    // If trn is empty or not all numeric, assign default value of "111111111"
                     ltrn[1] = "111111111";
                 }
 
                 var objtrn = obj.GetIndividualTrn(int.Parse(ltrn[1]));
                 if (objtrn != null && objtrn.IndividualInfo != null)
                 {
-                    sb.AppendLine(String.Format("{0},{1},{2},{3},{4},{5},{6}", line, objtrn.IndividualInfo.FirstName, objtrn.IndividualInfo.MiddleName, objtrn.IndividualInfo.LastName, objtrn.IndividualInfo.BirthDate.Value.ToShortDateString(), objtrn.IndividualInfo.GenderType, objtrn.IndividualInfo.NbrTrn));
+                    sb.AppendLine(string.Format("{0},{1},{2},{3},{4},{5},{6}", line, objtrn.IndividualInfo.FirstName, objtrn.IndividualInfo.MiddleName, objtrn.IndividualInfo.LastName, objtrn.IndividualInfo.BirthDate.Value.ToShortDateString(), objtrn.IndividualInfo.GenderType, objtrn.IndividualInfo.NbrTrn));
                 }
                 else
                 {
